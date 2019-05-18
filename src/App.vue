5b6484a76503a0de3ec01e0548aa99e9d937b967
honqiang.lee <honqiang.lee@gmail.com>
2019-05-18 22:07:46 +0800

    Lesson11

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,25 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hr>
-    <button class="btn btn-primary">确定</button>
-    <button class="btn btn-success">使用</button>
-    <button class="btn btn-danger">注意</button>
-    <hr>
-    <div class="myclass1">你好！</div>
-    <hr>
+    <p>
+      <!-- 使用 router-link 组件来导航. -->
+      <!-- 通过传入 `to` 属性指定链接. -->
+      <!-- <router-link> 默认会被渲染成一个 `<a>` 标签 -->
+      <router-link to="/">Home</router-link>
+      <router-link to="/news">新闻</router-link>
+      <router-link to="/about">关于</router-link>
+    </p>
+    <!-- 路由出口 -->
+    <!-- 路由匹配到的组件将渲染在这里 -->
     <router-view/>
   </div>
 </template>
 
 <script>
-import './assets/my.css'
+import "./assets/my.css";
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
 <style>
