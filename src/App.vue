2b953ebf89cca73bb3cbab8400c3feb8f6eda17d
honqiang.lee <honqiang.lee@gmail.com>
2019-05-18 19:59:41 +0800

    lesson3

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <hr>
+    <button class="btn btn-primary">确定</button>
+    <button class="btn btn-success">使用</button>
+    <button class="btn btn-danger">注意</button>
+    <hr>
     <router-view/>
   </div>
 </template>
@@ -13,7 +18,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
